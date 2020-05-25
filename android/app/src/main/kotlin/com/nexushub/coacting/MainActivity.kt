c94e956d200f0dcd82449415f76f8b184871700f
Lakeram Ramdhan <lakeramramdhan@gmail.com>
2020-05-25 10:41:29 -0400

    Initial Mobile Wrapper

---
+++ android/app/src/main/kotlin/com/nexushub/coacting/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nexushub.coacting
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/routes.dart
@@ -0,0 +1,9 @@
+import 'package:coacting/views/HomePage.dart';
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+final routes = {
+  '/home':  (BuildContext context) => new HomePage(),
+  '/' :     (BuildContext context) => new SplashScreen(),
+};
