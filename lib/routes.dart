2df3c21908af7829c5aba5fee862b77bf2f3a547
Lakeram Ramdhan <lakeramramdhan@gmail.com>
2020-05-25 15:24:54 -0400

    Reset Repo

--- lib/routes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:coacting/views/HomePage.dart';
-import 'package:flutter/material.dart';
-
-import 'main.dart';
-
-final routes = {
-  '/home':  (BuildContext context) => new HomePage(),
-  '/' :     (BuildContext context) => new SplashScreen(),
-};
